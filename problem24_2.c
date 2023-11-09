8a5db30930a47f97e34490a9515ecc0f688dc17e
lornop <lorenoelectric@gmail.com>
2023-11-08 19:08:42 -0800

    Need some work still Trying to treat the "number" as just an array and keep re-sorting it to add to the permutaion count

--- problem24_2.c
+++ problem24_2.c
@@ -10,17 +10,30 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 #include <stdio.h>
 int main()
 {
-    int 
+    long int 
     million                 = 1000000,
     permutation_count       = 0,
     digitsarray[10]         = {0,1,2,3,4,5,6,7,8,9},
-    digit                   = 0;
+    digit                   = 10,
+    digitcount              = 0;
 
 
-for (digit = 10; digit <= 10; digit --)
+    while (permutation_count < million)             //run until we get to the millionth permutation
     {
-        permutation_count++;
-        
+        //for (digit = 10; digit >= 0; digit --)      //change every digit in the array
+        if ( digit >= 0)
+        {
+            digit --;
+
+            digitcount = digit;
+
+            permutation_count++;
+
+            //move some numbers around in the array
+
+        }
+
+    printf("%d\n", permutation_count);
     }
 
 
