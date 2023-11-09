74a4bacf427e4919edb81819c6b8e356fd4b7a67
lornop <lorenoelectric@gmail.com>
2023-11-08 18:48:14 -0800

    Made a new program becuase the other one is f'd

--- problem24.c
+++ problem24.c
@@ -13,28 +13,28 @@ int arrange(int test, int digits[], int number[]); //Re-arrange the number
 
 int main()
 {
-int max = 1000000;
-int count = 0;
-int digits[10] = {0,1,2,3,4,5,6,7,8,9};
-int number[10];
-int n = 0;
-int test = 0;
-int value = 0;
+	int millionth = 1000000;
+	int count = 0;
+	int digits[10] = {0,1,2,3,4,5,6,7,8,9};
+	int number[10];
+	int n = 0;
+	int test = 0;
+	int value = 0;
 
 
-//***********************************************************
-for (test = 123456789; count <= 9876543210; test++)
-{
+	//***********************************************************
+	for (test = 123456789; count <= millionth; test++)
+	{
 	//number[] = arrange(test, digits[], number[]);
-	
-	
-	
-	//value = numberize(number[]);
-}
-printf("%d\n", value);
 
 
-return 0;
+
+	value = numberize(number[]);
+	}
+	printf("%d\n", value);
+
+
+	return 0;
 }
 
 
@@ -42,22 +42,20 @@ return 0;
 //************************************************************
 //This is the function to rearrange the digits
 int arrange(int test, int digits[], int number[])
-//
-
 {
-int available[10] = {1,1,1,1,1,1,1,1,1,1};
-int n = 0;
-int x = 0;
+	int available[10] = {1,1,1,1,1,1,1,1,1,1};
+	int n = 0;
+	int x = 0;
 
 
 
-for (n = 0; n < 10; n++)
-{
-	
-}
+	for (n = 0; n < 10; n++)
+	{
+		
+	}
 
 
-return 0;
+	return 0;
 }
 //***********************************************************
 
@@ -66,24 +64,22 @@ return 0;
 //************************************************************
 //This is the function to turn the digits into just one number
 int numberize(int number[])
-
-
 {
-int n, z = 0;
-int finalnum = 0;
-int exponent = 0;
+	int n, z = 0;
+	int finalnum = 0;
+	int exponent = 0;
 
-for (n = 0; n < 10; n++)
-{
-	exponent = 1;
-	for (z = (9 - n); z > 0; z--)
+	for (n = 0; n < 10; n++)
 	{
-		exponent = (exponent * 10);
+		exponent = 1;
+		for (z = (9 - n); z > 0; z--)
+		{
+			exponent = (exponent * 10);
+		}
+		
+		finalnum = finalnum + (number[n] * exponent);
 	}
-	
-	finalnum = finalnum + (number[n] * exponent);
-}
 
-return finalnum;
+	return finalnum;
 }
 //***********************************************************
