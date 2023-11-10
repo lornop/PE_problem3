b469da62fdd42c4f8430b01ed2a85fe713445620
lornop <lorenoelectric@gmail.com>
2023-11-10 13:37:41 -0800

    Gone back to the start. The second one i started is gone haywire also. Next plan is to write it like a sorting algorithm searching for the next smallest digit avaialable

--- problem24.c
+++ problem24.c
@@ -7,79 +7,45 @@ The lexicographic permutations of 0, 1 and 2 are:
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
+
 #include <stdio.h>
-int numberize(int number[]); // returns the digits in the array as just one int number
-int arrange(int test, int digits[], int number[]); //Re-arrange the number 
+//**********************************************
+
+//Functions
+
+//Global Variables
+
+__int32_t
+million 				= 1000000,
+permutation_count 		= 0,
+digits[10] 				= {0,1,2,3,4,5,6,7,8,9},
+permutation_array[10] 	= {0,1,2,3,4,5,6,7,8,9},
+permutation_index 		= 0,
+test_placeholder		= 0;
+
+//**********************************************
+//Start the Main loop
 
 int main()
 {
-	int millionth = 1000000;
-	int count = 0;
-	int digits[10] = {0,1,2,3,4,5,6,7,8,9};
-	int number[10];
-	int n = 0;
-	int test = 0;
-	int value = 0;
-
-
-	//***********************************************************
-	for (test = 123456789; count <= millionth; test++)
+	while (permutation_count < million)
 	{
-	//number[] = arrange(test, digits[], number[]);
-
-
-
-	value = numberize(number[]);
-	}
-	printf("%d\n", value);
-
-
-	return 0;
-}
-
-
-
-//************************************************************
-//This is the function to rearrange the digits
-int arrange(int test, int digits[], int number[])
-{
-	int available[10] = {1,1,1,1,1,1,1,1,1,1};
-	int n = 0;
-	int x = 0;
-
-
-
-	for (n = 0; n < 10; n++)
-	{
-		
-	}
-
-
-	return 0;
-}
-//***********************************************************
-
-
-
-//************************************************************
-//This is the function to turn the digits into just one number
-int numberize(int number[])
-{
-	int n, z = 0;
-	int finalnum = 0;
-	int exponent = 0;
-
-	for (n = 0; n < 10; n++)
-	{
-		exponent = 1;
-		for (z = (9 - n); z > 0; z--)
+		for(permutation_index = 9; permutation_index >=0; permutation_index--)
 		{
-			exponent = (exponent * 10);
+
 		}
-		
-		finalnum = finalnum + (number[n] * exponent);
 	}
 
-	return finalnum;
+	//If we are all done print out the millionth permutation
+	printf("The Millionth Permutation is : ");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", permutation_array[i]);
+	}
+	printf("\n");
+
+	return 0;
 }
-//***********************************************************
+
+
+//**********************************************
