57a779a1f12b105dc74705089a13d72f269d7fce
lornop <lorenoelectric@gmail.com>
2023-11-11 17:52:46 -0800

    another start over

--- problem24.c
+++ problem24.c
@@ -12,6 +12,13 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 //**********************************************
 
 //Functions
+void do_Permutation(__int32_t high_index);
+__int32_t find_lowest_number(__int32_t current_index);
+void add_permutation();
+__int32_t find_same_number_index(__int32_t, __int32_t);
+void print_permutation();
+
+
 
 //Global Variables
 
@@ -28,14 +35,68 @@ test_placeholder		= 0;
 
 int main()
 {
-	while (permutation_count < million)
-	{
-		for(permutation_index = 9; permutation_index >=0; permutation_index--)
-		{
 
+
+
+
+return 0;
+}
+
+
+//**********************************************
+void do_Permutation(__int32_t high_index)
+{
+
+	__int32_t 
+	working_index = high_index + 1,
+	temp_value = 9,
+	low_number;
+
+
+
+}
+
+
+
+//**********************************************
+void add_permutation()
+{
+	permutation_count++;
+}
+
+//**********************************************
+void bubbleSort(int arr[], int startIdx) {
+    int n = 10;
+	for (int i = startIdx; i < n - 1; i++) {
+        for (int j = startIdx; j < n - i - 1; j++) {
+            // Your sorting logic here
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+//**********************************************
+__int32_t find_same_number(__int32_t number_to_find, __int32_t starting_index)
+{
+
+	for (__int32_t n = starting_index; n < 10; n++)
+	{
+		if (number_to_find == permutation_array[n])
+		{
+			return n;
 		}
 	}
 
+}
+
+//**********************************************
+void print_permutation()
+{
 	//If we are all done print out the millionth permutation
 	printf("The Millionth Permutation is : ");
 	for (int i = 0; i < 10; i++)
@@ -43,9 +104,4 @@ int main()
 		printf("%d", permutation_array[i]);
 	}
 	printf("\n");
-
-	return 0;
 }
-
-
-//**********************************************
